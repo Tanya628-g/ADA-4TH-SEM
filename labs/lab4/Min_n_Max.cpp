7e58b1e79d16fc2da64e26462e422a36987c1892
Tanya628-g <tgandhi628@gmail.com>
2026-02-20 01:37:01 +0530

    Max and Min

--- labs/lab4/Min_n_Max.cpp
+++ labs/lab4/Min_n_Max.cpp
@@ -1,46 +1,57 @@
 #include<iostream>
+#include<chrono>
 using namespace std;
 
-void merge(int *arr, int low, int mid, int high){
-    int i = low;
-    int j = mid + 1;
-    int k = low;
-    int b[high - low + 1];
-    while(i <= mid && j <= high){
-        if(arr[i] < arr[j]){
-            b[k] = arr[i];
-            i++;
+const int MAX = 100;
+void MaxMin(int a[], int i, int j, int max, int min){
+    int mid, max1, min1;
+
+    if(i == j){
+        max = min = a[i];
+    }
+    else if(i == j - 1){
+        if(a[i] < a[j]){
+            max = a[j];
+            min = a[i];
         }
         else{
-            b[k] = arr[j];
-            j++;
+            max = a[i];
+            min = a[j];
         }
-        k++;
-    }  
-    if(i == mid + 1){
-        for(int x = mid + 1; x <= high; x++){
-            b[k] = arr[x];
-            k++;
-        }        
-    } 
+    }
     else{
-        for(int x = i; x <= mid; x++){
-            b[k] = arr[x];
-            k++;
+        mid = (i + j) / 2;
+        MaxMin(a, i, mid, max, min);
+        MaxMin(a, mid + 1, j, max1, min1);
+
+        if(max < max1){
+            max = max1;
         }
-    }
-    for(int x = low; x <= high; x++){
-        arr[x] = b[x - low];
-    }
-}
-void merge_sort(int *array, int a1, int an){
-    if(a1 < an){
-        int mid = (a1 + an) / 2;
-        merge_sort(array, a1, mid);
-        merge_sort(array, mid + 1, an);
-        merge(array, a1, mid, an);
+        if(min > min1){
+            min = min1;
+        }
     }
 }
 int main(){
+    int n;
+    int a[MAX];
+    int max, min;
 
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    srand(time(0));
+    cout<<"Enter elements:\n";
+    for(int i = 0; i < n; i++){
+        cin>>a[i];
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    MaxMin(a, 0, n-1, max, min);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::microseconds duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    
+
+    cout<<"\nMaximum element = "<<max<<endl;
+    cout<<"Minimum element = "<<min<<endl;
+
+    return 0;
 }

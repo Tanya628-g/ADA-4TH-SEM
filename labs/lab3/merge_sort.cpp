1e009b248b0bba608b7485a14ff87636db719465
Tanya628-g <tgandhi628@gmail.com>
2026-02-20 00:59:32 +0530

    Merge Sort

--- labs/lab3/merge_sort.cpp
+++ labs/lab3/merge_sort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void merge(int arr[], int low, int mid, int high){
     int i = low;
     int j = mid + 1;
-    int k = low;
+    int k = 0;
     int b[high - low + 1];
     while(i <= mid && j <= high){
         if(arr[i] < arr[j]){
@@ -18,29 +18,26 @@ void merge(int arr[], int low, int mid, int high){
         }
         k++;
     }  
-    if(i == mid + 1){
-        for(int x = mid + 1; x <= high; x++){
-            b[k] = arr[x];
-            k++;
-        }        
-    } 
-    else{
-        for(int x = i; x <= mid; x++){
-            b[k] = arr[x];
-            k++;
-        }
+    while(i <= mid){
+        b[k] = arr[i];
+        i++;
+        k++;
     }
-    for(int x = low; x <= high; x++){
-        arr[x] = b[x - low];
+    while(j <= high){
+        b[k] = arr[j];
+        j++;
+        k++;
+    }
+    for(int x = 0; x < k; x++){
+        arr[low + x] = b[x];
     }
 }
-void merge_sort(int arr[], int a1, int an){
-    if(a1 < an){
-        int mid = (a1 + an) / 2;
-        merge_sort(arr, a1, mid);
-        merge_sort(arr, mid + 1, an);
-        merge(arr, a1, mid, an);
-
+void merge_sort(int arr[], int low, int high){
+    if(low < high){
+        int mid = (low + high) / 2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
     }
 }
 int main(){
@@ -52,21 +49,25 @@ int main(){
     int mid, low, high;
     srand(time(0));
 
-    for (int n = min; n <= max; n++) {
+    for (int n = min; n <= max; n++){
         cout<<endl<<"unsorted array = ";
         int arr[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             arr[i] = rand() % 100;
             cout<< arr[i] <<" ";
         }
-        cout<<endl<<"sorted arrray = ";
         auto start = std::chrono::high_resolution_clock::now();
-        merge_sort(arr, 0, mid, n-1);
+        merge_sort(arr, 0, n-1);
         auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::microseconds duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        
+        cout<<endl<<"sorted arrray = ";
+        for(int i = 0; i < n; i++){
+            cout<<arr[i]<<" ";
+        }
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
 
-        cout<<endl<<"Time Taken = "<<duration.count()<<endl;
+        cout<<endl<<"Time Taken = "<<duration.count()<<" us"<<endl;
     }    
     return 0;
 }

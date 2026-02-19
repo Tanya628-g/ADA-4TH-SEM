d72a6be71deb0caa21493184f33e982001d53c69
Tanya628-g <tgandhi628@gmail.com>
2026-02-20 01:31:35 +0530

    Insertion sort

---
+++ labs/lab2/Insertion_sort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<chrono>
+using namespace std;
+const int max_size = 100;
+
+void insertion_sort(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main(){
+    int min, max;
+    cout<<"Enter minimum number of elements: ";
+    cin>>min;
+
+    cout<<"Enter maximum number of elements: ";
+    cin>>max;
+
+    srand(time(0));
+    for(int n = min; n <= max; n++){
+        cout<<"\nArray size = "<<n<<endl;
+        int arr[max_size];
+
+        cout<<"Unsorted array: ";
+        for(int i = 0; i < n; i++){
+            arr[i] = rand() % 100;
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+
+        auto start = chrono::high_resolution_clock::now();
+        insertion_sort(arr, n);
+        auto end = chrono::high_resolution_clock::now();
+        cout<<"Sorted array: ";
+        for(int i = 0; i < n; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        auto duration =chrono::duration_cast<chrono::nanoseconds>(end - start);
+
+        cout<<"Time taken: "<<duration.count()<<" ns"<<endl;
+    }
+
+    return 0;
+}

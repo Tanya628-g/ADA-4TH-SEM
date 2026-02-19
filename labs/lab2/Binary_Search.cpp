52c479d81d70022d087f636699731f16068d9f0d
Tanya628-g <tgandhi628@gmail.com>
2026-02-20 00:16:54 +0530

    Binary Search

--- labs/lab2/Binary_Search.cpp
+++ labs/lab2/Binary_Search.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
-#include<cmath>
+#include<chrono>  
 using namespace std;
-const int max_size = 100;
 
+const int max_size = 100;
 int binary_search(int arr[], int x, int low, int high){
     if(low > high){
         return -1;
     }
-    int mid = low + (high - low) / 2;
+    int mid = (low + high) / 2;
     if(arr[mid] == x){
         return mid;
     }
@@ -18,36 +18,42 @@ int binary_search(int arr[], int x, int low, int high){
         return binary_search(arr, x, low, mid-1);
     }
 }
-void initialise_arr(int arr[], int &n){
-    cout<<"enter number of digits"<<" ";
-    cin>>n;
-    cout<<"enter digits"<<endl;
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-}
-
-void display_arr(int arr[], int n){
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-
 int main(){
-    int n;
-    int arr[max_size];
-    initialise_arr(arr, n);
-    display_arr(arr, n);
     int x;
-    cout<<"enter element to search: ";
-    cin>>x;
-    int result = binary_search(arr, x, 0, n-1);
-    if(result != -1){
-        cout<<"element found at index "<<result<<endl;
-    }
-    else{
-        cout<<"element not found"<<endl;
+    cout<<"Enter element to search: ";
+    cin>>x;    
+    int min, max;
+    cout<<"Enter minimum number of elements: ";
+    cin>>min;
+    cout<<"Enter maximum number of elements: ";
+    cin>>max;
+
+    srand(time(0)); 
+    for(int n = min; n <= max; n++){
+        cout<<"\nArray size = "<<n<<endl;
+        int arr[max_size];
+        for(int i=0; i<n; i++){
+            arr[i] = rand() % 100;
+        }
+        sort(arr, arr+n);
+
+        cout<<"Sorted array: ";
+        for(int i=0; i<n; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        auto start = chrono::high_resolution_clock::now();
+        int result = binary_search(arr, x, 0, n-1);
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+
+        if(result != -1){
+            cout<<"Element found at index "<<result<<endl;
+        }
+        else{
+            cout<<"Element not found"<<endl;
+        }
+        cout<<"Time taken: "<<duration.count()<<" ns"<<endl;
     }
     return 0;
 }

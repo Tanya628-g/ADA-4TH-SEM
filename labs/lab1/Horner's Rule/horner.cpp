944a1dc5272899d8997faa05060ac8de9169d88a
Tanya628-g <tgandhi628@gmail.com>
2026-02-27 00:00:04 +0530

    .

--- labs/lab1/Horner's Rule/horner.cpp
+++ labs/lab1/Horner's Rule/horner.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <chrono>
+#include<fstream>
 using namespace std;
-// using namespace std::chrono;
+
 
 int hornersRule(int arr[], int n, int x) {
     int result = arr[0];
@@ -17,9 +18,11 @@ int main() {
     cout << "Enter maximum degree of polynomial: ";
     cin >> max;
     cout<<endl;
-    cout << "n\t time(us)\n";
+    // cout << "n\t time(us)\n";
     cout << endl;
 
+    ofstream fout("horner_rule.txt");
+    fout << "n time_us" << endl;
     for (int n = min; n <= max; n++) {
         int arr[100];
         for (int i = 0; i <= n; i++) {
@@ -31,9 +34,7 @@ int main() {
         auto end = std::chrono::high_resolution_clock::now();
 
         std::chrono::microseconds duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-        // int time_us = duration_cast<microseconds>(end - start).count();
-        // cout << n << "\t " << time_us << endl;
+        fout << n << " " << duration.count() << endl;
     }
     return 0;
 }

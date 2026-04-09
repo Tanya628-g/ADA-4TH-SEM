d28c3cc775b545df7e85c19f3cd13414acf80ff9
Tanya628-g <tgandhi628@gmail.com>
2026-04-09 19:29:27 +0530

    Dijkstra Path code

--- labs/lab6/Dijkstra_path/path.cpp
+++ labs/lab6/Dijkstra_path/path.cpp
@@ -1,45 +1,91 @@
 #include<iostream>
+#include<fstream>
+#include<chrono>
+#include<ctime>
 using namespace std;
 
-int min_heap(int arr[], int i){
-    int left=2 * i;
-    int right=(2*i +1);
-    int small;
-    if(arr[i] > arr[left]){
-        small = left;
+const int MAX = 100;
+
+int cost[MAX][MAX];
+int dist[MAX];
+int visited[MAX];
+int parent[MAX];
+
+void dijkstra(int n, int start){
+    int u = -1, w, min;
+    for(int i=1; i<=n; i++){
+        visited[i]= 0;
+        dist[i]= cost[start][i];
+        parent[i] = start;  
     }
-    else{
-        small=i;
-    }
-    if(arr[right] < arr[small]){
-        small=right;
-    }
-    if(small != i){
-        int temp=arr[small];
-        arr[small]=arr[i];
-        arr[small]=temp;
-        min_heap(arr[],small);
-    }
-}
-int dij(int e, int cost, int n){
-    for(int i; i<=n; i++){
-        visited[i]=0;
-        dist[i]=cost[v,i];
-    }
-    visited[u]=1;
-    for(int j=2; i<=n-1; j++){
-        min_heap();
-        visited[u]=1;
-    }
-    for(visit[w]==0){
-        if(dist[w]>dist[u] + cost[u,w]){
-            dist[w]=dist[u] + cost[u,w]
+    visited[start] = 1;
+    dist[start] = 0;
+    parent[start] = -1;
+
+    for(int j=2; j<=n; j++){
+        min = 999999;
+        for(int i=1; i<=n; i++){
+            if(dist[i] < min && visited[i] == 0){
+                min = dist[i];
+                u = i;
+            }
+        }
+        if(u == -1) break;
+        visited[u] = 1;
+        for(w=1; w<=n; w++){
+            if(!visited[w]){
+                if(dist[w] > dist[u] + cost[u][w]){
+                    dist[w]= dist[u] + cost[u][w];
+                    parent[w] = u;   // 🔥 update parent
+                }
+            }
         }
     }
-    return dist;
+}
+void printPath(int v){
+    if(v == -1) return;
+    printPath(parent[v]);
+    cout << v;
+    if(parent[v] != -1) cout << " -> ";
 }
 int main(){
-    int e;
-    cout<<"enter number of edges";
-    cin>>>e;
+    int min_el, max_el;
+    srand(time(0));
+    ofstream fout("dijkstra.txt");
+
+    cout<<"Enter minimum number of vertices: ";
+    cin>>min_el;
+    cout<< "Enter maximum number of vertices: ";
+    cin>>max_el;
+
+    fout<<"n time_ns\n";
+    for(int n=min_el; n <= max_el; n++){
+        cout<<"\nNumber of vertices = " << n << endl;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<= n; j++){
+                if(i==j)
+                    cost[i][j]= 0;
+                else
+                    cost[i][j]= rand() % 100 + 1; 
+            }
+        }
+        int start= rand() % n + 1;
+        cout<<"Starting vertex: "<<start<<endl;
+
+        auto start_time= chrono::high_resolution_clock::now();
+        dijkstra(n, start);
+        auto end_time= chrono::high_resolution_clock::now();
+        chrono::nanoseconds duration= chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
+
+        for(int i=1; i<=n; i++){
+            cout<<"Dist "<<start<<" -> "<<i<<" = "<<dist[i] << " | Path: ";
+            printPath(i);   
+            cout << endl;
+        }
+        cout<<"Time(ns): "<< duration.count()<< endl;
+        fout<<n<<" "<< duration.count()<<"\n";
+    }
+    fout.close();
+    return 0;
 }

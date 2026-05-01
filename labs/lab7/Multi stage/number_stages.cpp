2fa933a5cf9792c0fcf1cbc9df1025ffb3dcc265
Tanya628-g <tgandhi628@gmail.com>
2026-05-02 01:07:19 +0530

    Multi Stage

--- labs/lab7/Multi stage/number_stages.cpp
+++ labs/lab7/Multi stage/number_stages.cpp
@@ -1,110 +1,70 @@
 #include<iostream>
-#include<chrono>
-#include<fstream>
 using namespace std;
-
 #define INF 10000
-const int MAX = 50;
 
 int main(){
-    int min_v, max_v;
-    int arr[MAX][MAX];
-    int current[MAX], next[MAX];
-    int visited[MAX], indegree[MAX];
+    int v, e, v1, v2, w;
+    int stages = 1;
 
-    srand(time(0));
-    ofstream fout("Multi_stages.txt");
+    cout<<"Enter number of vertices: ";
+    cin>>v;
+    int arr[50][50];
+    int current[50], next[50];
+    int visited[50] = {0};
+    int indegree[50] = {0};
 
-    cout<<"Enter minimum number of vertices: ";
-    cin>>min_v;
-
-    cout<<"Enter maximum number of vertices: ";
-    cin>>max_v;
-    cout<<endl;
-
-    fout<<"n time_ns"<<endl;
-
-    for(int v = min_v; v <= max_v; v++){
-        cout<<"Vertices = "<<v<<endl;
-
-        for(int i = 0; i < v; i++){
-            visited[i] = 0;
-            indegree[i] = 0;
-
-            for(int j = 0; j < v; j++){
-                if(i == j){
-                    arr[i][j] = 0;
-                }
-                else{
-                    arr[i][j] = INF;
-                }
+    cout<<"Enter number of edges: ";
+    cin>>e;
+    for(int i=0; i<v; i++){
+        for(int j=0; j<v; j++){
+            if(i == j){
+                arr[i][j] = 0;
+            }
+            else{
+                arr[i][j] = INF;
             }
         }
-
-        // generate random DAG
-        for(int i = 0; i < v - 1; i++){
-            for(int j = i + 1; j < v; j++){
-                if(rand() % 2){
-                    arr[i][j] = rand() % 50 + 1;
-                    indegree[j]++;
-                }
-            }
-        }
-
-        int stages = 1;
-
-        auto start = chrono::high_resolution_clock::now();
-
-        int source = 0;
-
-        for(int i = 0; i < v; i++){
-            if(indegree[i] == 0){
-                source = i;
-                break;
-            }
-        }
-
-        int k = 1;
-        current[0] = source;
-        visited[source] = 1;
-
-        while(true){
-            int p = 0;
-
-            for(int i = 0; i < k; i++){
-                int node = current[i];
-
-                for(int j = 0; j < v; j++){
-                    if(arr[node][j] != 0 && arr[node][j] != INF && visited[j] == 0){
-                        next[p] = j;
-                        p++;
-                        visited[j] = 1;
-                    }
-                }
-            }
-
-            if(p == 0){
-                break;
-            }
-
-            for(int i = 0; i < p; i++){
-                current[i] = next[i];
-            }
-
-            k = p;
-            stages++;
-        }
-
-        auto end = chrono::high_resolution_clock::now();
-
-        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-
-        cout<<"Stages = "<<stages<<endl;
-        cout<<"Time taken(ns) = "<<duration.count()<<endl<<endl;
-
-        fout<<v<<" "<<duration.count()<<endl;
     }
+    cout<<"Enter edges (u v w):\n";
+    for(int i=0; i<e; i++){
+        cin>>v1>>v2>>w;
 
-    fout.close();
+        arr[v1][v2] = w;
+        indegree[v2]++;
+    }
+    int source = 0;
+    for(int i=0; i<v; i++){
+        if(indegree[i] == 0){
+            source = i;
+            break;
+        }
+    }
+    int k = 1;
+    current[0] = source;
+    visited[source] = 1;
+
+    while(true){
+        int p = 0;
+        for(int i=0; i<k; i++){
+            int node = current[i];
+
+            for(int j=0; j<v; j++){
+                if(arr[node][j] != 0 && arr[node][j] != INF && visited[j] == 0){
+                    next[p] = j;
+                    p++;
+                    visited[j] = 1;
+                }
+            }
+        }
+        if(p == 0){
+            break;
+        }
+        for(int i=0; i<p; i++){
+            current[i] = next[i];
+        }
+        k = p;
+        stages++;
+    }
+    cout<<"Number of stages: "<<stages;
     return 0;
 }
